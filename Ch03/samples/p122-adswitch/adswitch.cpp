e47d4ece30f2bb88e3100203ae023a27a6ccfe90
Vital Teresh <vster2006@gmail.com>
2017-05-11 18:05:09 +0300

    add p120-p122

--- Ch03/samples/p122-adswitch/adswitch.cpp
+++ Ch03/samples/p122-adswitch/adswitch.cpp
@@ -1,15 +1,58 @@
 // adswitch.cpp
 // приключенческая игра с использованием switch
 #include <iostream>
+#include <termios.h>
+#include <stdio.h>
+
 using namespace std;
-#include <conio.h>           // для getche()
+
+static struct termios old,nov;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  nov = old; /* make new settings same as old settings */
+  nov.c_lflag &= ~ICANON; /* disable buffered i/o */
+  nov.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &nov); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+  return getch_(1);
+}
+
 
 int main()
 {
 	char dir = 'a';
 	int x = 10, y = 10;
 	
-	while( dir !='\r' )
+    while( dir !='\n' )
 	{
 		cout << "\nYour coords: " << x << ", " << y;
 		cout << "\nChoose direction (n,s,e,w): ";
@@ -20,7 +63,7 @@ int main()
 			case 's': y++; break;  	// движение на юг
 			case 'e': x++; break;  	// движение на восток
 			case 'w': x--; break;  	// движение на запад
-			case '\r': cout << "\nExit...\n" ; break; // нажатие Enter
+            case '\n': cout << "\nExit...\n" ; break; // нажатие Enter
 			default: cout << "\nTry again\n";     // нажатие других клавиш
 			}                        // конец switch
 	}                          		// конец while
