1d595016a030d12366a2d099a01458b81541a4f3
Vital Teresh <vster2006@gmail.com>
2017-05-11 17:38:24 +0300

    add p117-p119

--- Ch03/samples/p119-adelseif/adelseif.cpp
+++ Ch03/samples/p119-adelseif/adelseif.cpp
@@ -1,8 +1,51 @@
 // adelseif.cpp
 // приключенческая игра с применением else...if
 #include <iostream>
+#include <termios.h>
+#include <stdio.h>
+
 using namespace std;
-#include <conio.h>           // для getche()
+
+static struct termios old,nov;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  nov = old; /* make new settings same as old settings */
+  nov.c_lflag &= ~ICANON; /* disable buffered i/o */
+  nov.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &nov); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+  return getch_(1);
+}
+
 
 int main()
 {
@@ -10,7 +53,7 @@ int main()
 	int x = 10, y = 10;
 	
 	cout << "Press Enter for exit...\n";
-	while( dir != '\r' )			// пока не нажата клавиша Enter
+    while( dir != '\n' )			// пока не нажата клавиша Enter
 	{
 		cout << "\nYour coords: " << x << ", " << y;
 		cout << "\nChoose direction (n,s,e,w): ";
