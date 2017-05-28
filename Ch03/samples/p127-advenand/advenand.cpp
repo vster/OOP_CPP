168cf902f0857fcacf0d8c65ee6b724db51f7fd7
Vital Teresh <vster2006@gmail.com>
2017-05-28 10:56:58 +0300

    add p126, p127

--- Ch03/samples/p127-advenand/advenand.cpp
+++ Ch03/samples/p127-advenand/advenand.cpp
@@ -2,14 +2,59 @@
 // применение операции логического »
 #include <iostream>
 using namespace std;
-#include <process.h>         // дл€ exit()
-#include <conio.h>           // дл€ getche()
+// #include <process.h>         // дл€ exit()
+// #include <conio.h>           // дл€ getche()
+#include <termios.h>
+#include <stdio.h>
+
+using namespace std;
+
+static struct termios old,nov;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  nov = old; /* make new settings same as old settings */
+  nov.c_lflag &= ~ICANON; /* disable buffered i/o */
+  nov.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &nov); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+  return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+  return getch_(1);
+}
+
 
 int main()
 {
 	char dir = 'a';
 	int x = 10, y = 10;
-	while ( dir != '\r ' )
+    while ( dir != '\n ' )
 		{
 		cout << "\nYour coords: " << x << ", " << y;
 		cout << "\nChoose direction (n,s,e,w): ";
